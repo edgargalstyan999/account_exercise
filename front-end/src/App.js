a009f9337f6cebc1184dbd9ac80049e88ffb2a7e
edgargalstyan999 <edgargalstyan999@gmail.com>
2021-12-22 19:47:24 +0400

    created table html  with data object

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,62 @@
 import React, { useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import './index.css';
 
+const users = [
+  {
+    id: 1,
+    name: 'Account 1',
+    created: '5-8-2021',
+    owner: 'User 1',
+    action: 'Action',
+  },
+  {
+    id: 2,
+    name: 'Account 2',
+    created: '5-8-2021',
+    owner: 'User 2',
+    action: 'Action',
+  },
+  {
+    id: 3,
+    name: 'Account 3',
+    created: '5-8-2021',
+    owner: 'User 3',
+    action: 'Action',
+  },
+];
 
 function App() {
-  
+
+
+
+    
+
+  return (
+    <div>
+      <div className='comp_table'>
+      <table>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Created On</th>
+            <th>Owner</th>
+            <th>Action</th>
+          </tr>
+          {users.map((getUsers) => (
+            <tr>
+              <th>{getUsers.id}</th>
+              <th>{getUsers.name}</th>
+              <th>{getUsers.created}</th>
+              <th>{getUsers.owner}</th>
+              <th>
+                <a href={getUsers.action}>View</a>
+              </th>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
 }
 export default App;

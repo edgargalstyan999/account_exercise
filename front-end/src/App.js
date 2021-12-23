a2059561b114b5cb8bd99a58b3c177ec2f811c1c
edgargalstyan999 <edgargalstyan999@gmail.com>
2021-12-23 13:06:23 +0400

    added  user and tables components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,62 +1,17 @@
-import React, { useState } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
-
-const users = [
-  {
-    id: 1,
-    name: 'Account 1',
-    created: '5-8-2021',
-    owner: 'User 1',
-    action: 'Action',
-  },
-  {
-    id: 2,
-    name: 'Account 2',
-    created: '5-8-2021',
-    owner: 'User 2',
-    action: 'Action',
-  },
-  {
-    id: 3,
-    name: 'Account 3',
-    created: '5-8-2021',
-    owner: 'User 3',
-    action: 'Action',
-  },
-];
+import { Tables } from './Tables';
+import { User } from './User';
 
 function App() {
-
-
-
-    
-
   return (
-    <div>
-      <div className='comp_table'>
-      <table>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Created On</th>
-            <th>Owner</th>
-            <th>Action</th>
-          </tr>
-          {users.map((getUsers) => (
-            <tr>
-              <th>{getUsers.id}</th>
-              <th>{getUsers.name}</th>
-              <th>{getUsers.created}</th>
-              <th>{getUsers.owner}</th>
-              <th>
-                <a href={getUsers.action}>View</a>
-              </th>
-            </tr>
-          ))}
-        </table>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Tables />} />
+        <Route path='/accounts/:id' element={<User />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;

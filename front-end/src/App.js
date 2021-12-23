65abde0f0fb008f3d2ab97cf009ba56d2af9cce6
edgargalstyan999 <edgargalstyan999@gmail.com>
2021-12-23 14:38:48 +0400

    deleting

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
-import { Tables } from './Tables';
-import { User } from './User';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Tables />} />
-        <Route path='/accounts/:id' element={<User />} />
-      </Routes>
-    </Router>
-  );
-}
+
+
 export default App;
